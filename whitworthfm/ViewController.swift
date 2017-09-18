e82546e59c7adfc24a8d6f1860540a61db48d667
Michael Lindahl <me@michaellindahl.com>
2017-09-17 20:42:52 -0700

    Play stream without VC or user interaction

--- whitworthfm/ViewController.swift
+++ whitworthfm/ViewController.swift
@@ -11,36 +11,23 @@ import AVKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    var streamPlayer: AVPlayer = {
+        let player = AVPlayer(url: URL(string: "http://majestic.wavestreamer.com:7985/live.mp3")!)
 
+        return player
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        play()
+        streamPlayer.play()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    func play() {
-        guard let url = URL(string: "http://majestic.wavestreamer.com:7985/live.mp3") else {
-            return
-        }
-        // Create an AVPlayer, passing it the HTTP Live Streaming URL.
-        let player = AVPlayer(url: url)
-        
-        // Create a new AVPlayerViewController and pass it a reference to the player.
-        let controller = AVPlayerViewController()
-        controller.player = player
-        
-        // Modally present the player and call the player's play() method when complete.
-        present(controller, animated: true) {
-            player.play()
-        }
-    }
-
 
 }
 
